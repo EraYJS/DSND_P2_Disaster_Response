8b3c38f5ba6b4164e374e6265069d571c531a86a
Jinsheng Yuan <orange.ariza@gmail.com>
2020-04-17 12:45:53 +0800

    WiP - ML pipeline: add main function and grid search result in comments.
    Signed-off-by: Jinsheng Yuan <orange.ariza@gmail.com>

--- ml_ppl.py
+++ ml_ppl.py
@@ -4,14 +4,14 @@ import pandas as pd
 import pickle
 import re
 
-from nltk import pos_tag
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import sent_tokenize, word_tokenize
 
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline, FeatureUnion
 
@@ -20,10 +20,20 @@ from sqlalchemy import create_engine
 
 def arg_parse():
     parser = ap.ArgumentParser()
-    parser.add_argument("-p", action="store",
-                        default="disaster_sn_msg", dest="db_path")
-    parser.add_argument("-p", action="store",
+    parser.add_argument("-d", action="store", required=True,
+                        default="disaster_sn_msg.db", dest="db_pth")
+    parser.add_argument("-m", action="store", required=True,
                         default="model.pkl", dest="md_pth")
+    parser.add_argument("-e", action="store", required=False,
+                        default=True, dest="eval")
+    parser.add_argument("-g", action="store", required=False,
+                        default=False, dest="gs")
+    parser.add_argument("-t", action="store", required=False,
+                        default="XGB", dest="clf_type")
+
+    args = parser.parse_args()
+
+    return args
 
 
 def load_db(path):
@@ -55,43 +65,48 @@ def msg_tokenize(text):
     return words_lemmed
 
 
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+def build_ppl(clf_type):
     """
-    Starting Verb Extractor class
+    AdaBoost GridSearch
+    {'clf__estimator__learning_rate': 1.0,
+     'clf__estimator__n_estimators': 55,
+     'vect__max_df': 0.75}
 
-    This class extract the starting verb of a sentence,
-    creating a new feature for the ML classifier
+    XGBoost GridSearch
+    {'clf__estimator__colsample_bytree': 1.0,
+     'clf__estimator__gamma': 5.0,
+     'clf__estimator__learning_rate': 0.5,
+     'clf__estimator__min_child_weight': 1,
+     'clf__estimator__subsample': 1.0,
+     'vect__max_df': 0.75}
     """
+    ada_ppl = Pipeline([('vect', CountVectorizer(
+                            tokenizer=msg_tokenize,
+                            max_df=0.75,)),
+                        ('tfidf', TfidfTransformer()),
+                        ('clf', MultiOutputClassifier(
+                            AdaBoostClassifier(
+                                learning_rate=1.0,
+                                n_estimators=55)))
+                        ])
 
-    def starting_verb(self, text):
-        sentence_list = sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = pos_tag(msg_tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
+    xgb_ppl = Pipeline([('vect', CountVectorizer(
+                            tokenizer=tokenize,
+                            max_df=0.75)),
+                        ('tfidf', TfidfTransformer()),
+                        ('clf', MultiOutputClassifier(
+                            XGBClassifier(
+                                colsample_bytree=1.0,
+                                gamma=5.0,
+                                learning_rate=0.5,
+                                min_child_weight=1,
+                                subsample=1.0)))
+                        ])
 
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-
-
-def build_ppl():
-    ppl = Pipeline([
-            ('features', FeatureUnion([
-                    ('text_pipeline', Pipeline([
-                            ('vect', CountVectorizer(tokenizer=msg_tokenize)),
-                            ('tfidf', TfidfTransformer())
-                    ])),
-
-                    ('starting_verb', StartingVerbExtractor())
-            ]))])
-
-    return ppl
+    if clf_type == "ADA":
+        return ada_ppl
+    elif clf_type == "XGB":
+        return xgb_ppl
 
 
 def model_save(model, path):
@@ -100,7 +115,75 @@ def model_save(model, path):
     pass
 
 
+def perf_eval(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    ytest = np.array(y_test)
+    sample_label_recall = []
+    label_precision = []
+
+    for i in range(y_test.shape[0]):
+        if ytest[i].sum() != 0:
+            sample_label_recall.append(np.bitwise_and(
+                    y_pred[i], ytest[i]).sum() / ytest[i].sum())
+        elif ytest[i].sum() == 0:
+            sample_label_recall.append(1)
+
+    for j in range(y_test.shape[1]):
+        label_precision.append(np.invert(
+                np.logical_xor(y_pred[:, j],
+                               ytest[:, j])).sum() / ytest.shape[0])
+
+    slr_mean = np.array(sample_label_recall).mean()
+    lp_mean = np.array(label_precision).mean()
+
+    f1_score = 2 * slr_mean * lp_mean / (slr_mean + lp_mean)
+
+    return sample_label_recall, label_precision, f1_score
+
+
+def gs_eval(y_test, y_pred):
+    ytest = np.array(y_test)
+    sample_label_recall = []
+    label_precision = []
+
+    for i in range(y_test.shape[0]):
+        if ytest[i].sum() != 0:
+            sample_label_recall.append(np.bitwise_and(
+                    y_pred[i], ytest[i]).sum() / ytest[i].sum())
+        elif ytest[i].sum() == 0:
+            sample_label_recall.append(1)
+
+    for j in range(y_test.shape[1]):
+        label_precision.append(np.invert(
+                np.logical_xor(y_pred[:, j],
+                               ytest[:, j])).sum() / ytest.shape[0])
+
+    slr_mean = np.array(sample_label_recall).mean()
+    lp_mean = np.array(label_precision).mean()
+    f1_score = 2 * slr_mean * lp_mean / (slr_mean + lp_mean)
+
+    return f1_score
+
+
+
 def ml_main():
+    args = arg_parse()
+
+    X, y = load_db(args.db_pth)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+    pipeline = build_ppl(args.clf_type)
+
+    pipeline.fit(X_train, y_train)
+
+    if args.eval:
+        slr_mean, lp_mean, f1_score = perf_eval(pipeline, X_test, y_test)
+
+        # TODO: plot scores
+
+    model_save(pipeline, args.md_pth)
+
     pass
 
 
